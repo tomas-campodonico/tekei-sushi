fd471f107cf5c6f3d50b263f6818d77e160a88b4
Tomas Campodonico <tomycsl@gmail.com>
2017-04-02 21:09:13 -0300

    Config test mailer

--- config/env/test.js
+++ config/env/test.js
@@ -32,12 +32,12 @@ module.exports = {
 		callbackURL: '/auth/github/callback'
 	},
 	mailer: {
-		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		from: process.env.MAILER_FROM || '"Tekei Sushi [Test]" <qa.tekei-sushi@hotmail.com>',
 		options: {
-			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			service: process.env.MAILER_SERVICE_PROVIDER || 'Hotmail',
 			auth: {
-				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+				user: process.env.MAILER_EMAIL_ID || 'qa.tekei-sushi@hotmail.com',
+				pass: process.env.MAILER_PASSWORD || 'Tekei/123456'
 			}
 		}
 	}
